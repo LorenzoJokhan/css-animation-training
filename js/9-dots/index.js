46a1ce83dc853e4b3b5ed7751569496bb27beb13
Lorenzo Jokhan <lorenzoluk@gmail.com>
2025-04-09 22:01:11 +0200

    Added js animations

---
+++ js/9-dots/index.js
@@ -0,0 +1,5 @@
+const navigation = document.querySelector('.navigation');
+
+navigation.addEventListener('click', (e) => {
+    navigation.classList.toggle('active');
+});
---
+++ js/toggle-action-menu/index.js
@@ -0,0 +1,5 @@
+let navigation  = document.querySelector('.navigation');
+console.log(navigation);
+navigation.addEventListener('click', function () {
+  navigation.classList.toggle('active');
+})
